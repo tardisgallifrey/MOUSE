84b2149de06bfad33210013b26143bc64cd6e611
Dave <bigmanrunning@gmail.com>
2020-09-07 18:55:31 -0500

    C++ removed struct for array stack 09/07/2020

--- cplusplus/mouse2.cpp
+++ cplusplus/mouse2.cpp
@@ -16,21 +16,15 @@ using namespace std;
 #define MAXPROGLEN 1000
 #define STACKSIZE 20
 
-
-
-struct stack
-{
-   int data;
-   struct stack *ptr; //pointer type of stack
-};
-
-typedef struct stack Stack;
-typedef Stack *stackPtr;
+//Define stack and pointer
+int stack_num[STACKSIZE];
+int stackpointer = -1;
 
 char readprog(int charpos, char *prog);
-void push( stackPtr *top, int x );
-int pop(stackPtr *top);
+int backpos(int charpos);
 int ctoi(char ch);
+int push(int val);
+int pop();
 
 
 int main(int argc, char *argv[])                        //get mouse program file from command line
@@ -38,17 +32,15 @@ int main(int argc, char *argv[])                        //get mouse program file
 
     //Variable Declarations
     ifstream filePointer;
-    char ch;                                        //read character
+    char ch;                                            //read character
     char *is_it_mouse;
     char *filename = (char*)malloc(100);
-    char *prog = (char*)malloc(MAXPROGLEN);         //program array of characters
-    int *stack = (int*)malloc(STACKSIZE);
-    int charpos = 0;                                //character position in array
+    char *prog = (char*)malloc(MAXPROGLEN);             //program array of characters
+    int charpos = 0;                                    //character position in array
     int temp;
-    stackPtr stackNewPtr = NULL;                    //declared pointer that points to the top of stack
     string line;
 
-       //What to do if command line is deficient
+    //What to do if command line is deficient
     if(argc != 2){                                      //check to see if a file name is included
         cout << "\nUsage: mouse hello.mouse" << endl;
         cout << "     Also, be sure file ends in .mouse" << endl;
@@ -100,6 +92,7 @@ int main(int argc, char *argv[])                        //get mouse program file
     }
     //close file pointer
     filePointer.close();
+    //cout << prog << endl;
 
     //*******Begin Interpretation of MOUSE Program********
     
@@ -129,36 +122,40 @@ int main(int argc, char *argv[])                        //get mouse program file
                     charpos++;
                     ch = readprog(charpos, prog);
                 }while(isdigit(ch));
-                push(&stackNewPtr, temp);  
-                charpos -= 1;
+                push(temp);  
+                backpos(charpos);
                 break;
             case '+':
-                push(&stackNewPtr, pop(&stackNewPtr) + pop(&stackNewPtr) );
+                push(pop() + pop());
                 break;
             case '-':
-                temp = pop(&stackNewPtr);
-                push(&stackNewPtr, pop(&stackNewPtr) - temp );
+                temp = pop();
+                push( pop() - temp);
                 break;
             case '*':
-                push(&stackNewPtr, pop(&stackNewPtr) * pop(&stackNewPtr));
+                push( pop() * pop() );
                 break;
             case '/':
-                temp = pop(&stackNewPtr);
-                push(&stackNewPtr, pop(&stackNewPtr) / temp);
+                temp = pop();
+                push( pop() / temp);
                 break;
             case '%':
-                temp = pop(&stackNewPtr);
-                push(&stackNewPtr, pop(&stackNewPtr) % temp);
+                temp = pop();
+                push( pop() % temp);
                 break;
             case '!':
-                printf("%d", pop(&stackNewPtr));
+                cout << pop() << endl;
                 break;
+            case '?':
+                cin >> temp;
+                push(temp);
             default:
                 break;
 
       }
       charpos++;
     }
+    cout << endl;
 }
 
 
@@ -174,43 +171,7 @@ int backpos(int charpos){
 
 }
 
-//function for pushing value into the top of stack
-void push( stackPtr *top, int x )
-{
-    stackPtr nodePtr;
 
-    //allocating memory
-    nodePtr = (stackPtr)malloc(sizeof(Stack));
-
-    //pushing value at the top of stack only if there is space
-    if(nodePtr != NULL)
-    {
-       nodePtr -> data = x;
-       nodePtr -> ptr = *top;
-       *top = nodePtr;
-    }
-
-    else
-       printf("\nERROR !!! (Not enough space)");
-}
-
-//popping value out of the stack
-int pop(stackPtr *top)
-{
-   int pValue; //variable for value at the top
-   stackPtr tempPtr; //temporary pointer
-
-   tempPtr = *top;
-   pValue = (*top) -> data;
-   *top = (*top) -> ptr;
-   free(tempPtr); //free temporary pointer value
-   return pValue;
-}
-
-int checkEmpty(stackPtr top)
-{
-   return top == NULL;
-}
 
 //convert number character to digit
 int ctoi(char ch){
@@ -218,5 +179,37 @@ int ctoi(char ch){
     int digit = ch - '0';
     if(digit >= 0 && digit <= 9){
         return digit;
+    }
+}
+
+//push a number onto the stack_num
+int push(int val) 
+{
+   if(stackpointer >= STACKSIZE-1)
+   {
+        return 0;
+   } 
+   else 
+   {
+      stackpointer++;
+      stack_num[stackpointer]=val;
+      return 1;
+   }
+}
+
+//pop a number off of the stack_num
+int pop() 
+{
+    int n;
+    if(stackpointer <= -1)
+    {
+        cout << "Stack Underflow.  Quitting."<<endl;
+        exit(-1);
+    }
+    else 
+    {
+       n = stack_num[stackpointer]; 
+       stackpointer--;
+       return n;
     }
 }
